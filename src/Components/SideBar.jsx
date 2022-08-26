a90ce3cccd4a92dea6f549cff1623ff0a8cc3497
unknown <virajithaabekoon@gmail.com>
2022-08-26 15:45:19 +0530

    Common Stat

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,7 +4,7 @@ import {useNavigate} from 'react-router-dom'
 
 export default function SideBar() {
 
-    nav  = useNavigate()
+    const nav  = useNavigate()
 
     const LogoutFunc = () =>{
         sessionStorage.clear()
