fc1772acabee417c190e5b95460850e26419c198
unknown <virajithaabekoon@gmail.com>
2022-08-26 15:45:01 +0530

    Log Out

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,8 +1,15 @@
 import jwt_decode from 'jwt-decode'
 import React, { useEffect, useState } from 'react'
-
+import {useNavigate} from 'react-router-dom'
 
 export default function SideBar() {
+
+    nav  = useNavigate()
+
+    const LogoutFunc = () =>{
+        sessionStorage.clear()
+        nav('/')
+    }
     
     const [RawData,SetRawData] = useState({})
 
@@ -26,7 +33,7 @@ export default function SideBar() {
                         <li className="nav-item dropdown dropdown-sm">
                             <img width={50} src={RawData?.picture} className="nav-link dropdown-toggle rounded-circle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"/>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">Log Out<i class="fa-solid fa-power-off mx-3"></i></a></li>
+                                <li><a onClick={LogoutFunc} className="dropdown-item" href="#">Log Out<i class="fa-solid fa-power-off mx-3"></i></a></li>
                             </ul>
                         </li>
                     </ul>
